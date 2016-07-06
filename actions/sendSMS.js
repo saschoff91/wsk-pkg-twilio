19e3e7bffb349455f3d56f5c26d45c836620e353
saschoff91 <saschoff@de.ibm.com>
2016-07-06 18:01:08 +0200

    Update sendSMS.js

--- actions/sendSMS.js
+++ actions/sendSMS.js
@@ -37,53 +37,5 @@ function main(params) {
 	    return whisk.done("SMS sent successful, sid: "+ message.sid);
 	});
 	
-	
-	
-	//----------------- RESTFUL API BASED SMS TRANSMITTING
-	
-	/*var uri = "https://"+accountSid+":"+authToken+"@"+base+'/Accounts/'+accountSid+'/Messages.json';
-
-	//testing parameter input
-	/*
-	var form = {		
-			To: "+491721366239", 
-			From: "+4915735984041", 
-			Body: "WAZZZUUUUUUPPPP2?!!!!"   
-	};*/
-
-	/*
-	if (messagingServiceSid) {
-		console.log("messagingServiceSid is set: "+ messagingServiceSid);
-		var form = {		
-				To: to, 
-				Body:message,
-				MessagingServiceSid: messagingServiceSid
-		};   
-	} else {
-		var form = {		
-				To: to, 
-				From: from, 
-				Body:message  
-		};
-	}
-
-	var req = request.post({
-		uri:uri,
-		form:form
-	}, function(error, response,body) {
-		if ( response.statusCode == 201) {
-			console.log(body);
-		}
-	});
-
-	req.on('response', function(response) {
-		if ( response.statusCode == 201) {
-			return whisk.done(response);
-		} else {
-			return whisk.error(response);
-		}
-
-	});*/
-
 	return whisk.async();
 } 
