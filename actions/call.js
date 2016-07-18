e3b81af5946d94f3f8428c7694dc56990b6e8798
saschoff91 <saschoff@de.ibm.com>
2016-07-18 17:54:59 +0200

    Add files via upload

--- actions/call.js
+++ actions/call.js
@@ -1,57 +1,93 @@
 /*
- * Action sendSMS based on twilio
- * Hava a look at the restful api documentation from twilio
- * https://www.twilio.com/docs/api/rest/making-calls 
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 var request = require('request');
 
+/**
+ * An action to perform a automatic call from Twilio service API
+ * @params      {string}  twilioBase                (required)  Twilio API endpoint,
+ * @params      {string}  accountSID                (required)  Twilio account SID
+ * @params      {string}  authToken                 (required)  Twilio account authentication token
+ * @params      {string}  to                    	(required)  Receiver mobile number 
+ * @params      {string}  from              		(required)  Twilio transmitter number
+ * @params      {string}  url              			(required)  URL of voice message, XML file
+ * @return      {Object}                                        Done with the result of invocation
+ **/
+
 function main(params) {
 	console.log(params);
 
-	// twilio credentials from package binding
-	var base = params.base;
-	var accountSid = params.account;
-	var authToken = params.token;
+	var requiredParams = ["twilioBase", "accountSID", 'authToken', 'to', 'from', 'url'];
 
-	//mandatory param input from cli
-	var to = params.to;
-	var from = params.from;
-	var voiceUrl = params.url;
+	checkParameters(params, requiredParams, function(missingParams) {
+		if (missingParams != "") {
+			console.error("Missing required parameters: " + missingParams);
+			return whisk.error("Missing required parameters: " + missingParams);
+		} else {
+			var url = "https://"+params.accountSID+":"+params.authToken+"@"+params.twilioBase+'/Accounts/'+params.accountSID+'/Calls.json';
 
-	//optional param input from cli
-	var applicationSid = params.applicationSid; //alternative to url
+			var form = {		
+					To: params.to, 
+					From: params.from, 
+					Url: params.url   
+			};
 
-	var uri = "https://"+accountSid+":"+authToken+"@"+base+'/Accounts/'+accountSid+'/Calls.json';
+			var options = {
+					method: 'POST',
+					url: url,
+					form: form
+			};
 
-	if (applicationSid) {
-		var form = {		
-				To: to, 
-				From: from, 
-				ApplicationSid: applicationSid   
-		};
-	} else {
-		var form = {		
-				To: to, 
-				From: from, 
-				Url: voiceUrl   
-		};
-	}
+			request(options, function(err, res, body) {
+				if (!err && res.statusCode == 201) {
+					var parsedBody = JSON.parse(body);
+					return whisk.done(parsedBody);
 
-	var req = request.post({
-		uri:uri,
-		form:form
-	});
-
-	req.on('response', function(response) {
-		if ( response.statusCode == 201) {
-			return whisk.done(response);
+				} else {
+					return whisk.error({
+						statusCode: (res || {}).statusCode,
+						error: err,
+						body: body
+					});
+				}
+			});
 		}
-		else {
-			return whisk.error(response);
-		}
-
 	});
-
 	return whisk.async();
 } 
+
+/**
+ *  A function that check whether the parameters passed are required or not
+ *
+ * @param      {object}    params    An object contains the parameter required
+ *                                   in order to check it and generate a string
+ *                                   that contains a list of missing parameters
+ * @param      {Function}  callback  the callback function has the generated
+ *                                   array or an empty one if the params is
+ *                                   empty or nothing is missing
+ */
+function checkParameters(params, requiredParams, callback) {
+	console.log("Checking Existence of Required Parameters");
+	var missingParams = [];
+	for (var i = requiredParams.length - 1; i >= 0; i--) {
+		console.log(requiredParams[i]);
+		if (!params.hasOwnProperty(requiredParams[i])) {
+			missingParams.push(requiredParams[i]);
+		}
+		if (i == 0)
+			return callback(missingParams);
+	}
+}
--- actions/sendSMS.js
+++ actions/sendSMS.js
@@ -1,41 +1,114 @@
 /*
- * Action sendSMS based on twilio 
- * Have a look at the restful api from twilio
- * https://www.twilio.com/docs/api/rest/sending-messages
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+var request = require('request');
+
+/**
+ * An action to send a text message via Twilio service API.
+ * @params      {string}  twilioBase                (required)  Twilio API endpoint,
+ * @params      {string}  accountSID                (required)  Twilio account SID
+ * @params      {string}  authToken                 (required)  Twilio account authentication token
+ * @params      {string}  to                    	(required)  Receiver mobile number 
+ * @params      {string}  from              		(required)  Twilio transmitter number
+ * @params      {string}  message              		(required)  Text message to send
+ * @return      {Object}                             			Done with the result of invocation
+ **/
+
 function main(params) {
-
-	var request = require('request');
-
-	var twilio = require('twilio');
-	// twilio credentials from package binding
-	var base = params.base;
-	var accountSid = params.account;
-	var authToken = params.token;
-
-	//required param input from cli
-	var to = params.to;
-	var from = params.from;
-	var message = params.message;
-
-	//optional parameter input, alternativ to 'from'
+	//optional parameter, alternative to 'from'
 	var messagingServiceSid = params.messagingServiceSid;
 
-	//var accountSid = {{ accountSid }}; // Your Account SID from www.twilio.com/console
-	//var authToken = {{ authToken }};   // Your Auth Token from www.twilio.com/console
+	console.log(params);
 
-	var twilio = require('twilio');
-	var client = new twilio.RestClient(accountSid, authToken);
+	var requiredParams = ["twilioBase", "accountSID", 'authToken', 'to', 'message'];
 
-	client.messages.create({
-	    body: message,
-	    to: to,  // Text this number
-	    from: from // From a valid Twilio number
-	}, function(err, message) {
-	    console.log(message.sid);
-	    return whisk.done("SMS sent successful, sid: "+ message.sid);
+	checkParameters(params, requiredParams, function(missingParams) {
+		if (missingParams != "") {
+			console.error("Missing required parameters: " + missingParams);
+			return whisk.error("Missing required parameters: " + missingParams);
+		} else {
+
+			if ((!params.from)&&(!params.messagingServiceSid)) {
+				return whisk.error("Missing required parameters: 'from' and 'messagingServiceSid' are missing. At least one of them must given!");
+			} 
+
+			var url = "https://"+params.accountSID+":"+params.authToken+"@"+params.twilioBase+'/Accounts/'+params.accountSID+'/Messages.json';
+
+			if (params.messagingServiceSid) {
+				console.log("messagingServiceSid is set: "+ params.messagingServiceSid);
+				var body = {		
+						To: params.to, 
+						Body: params.message,
+						MessagingServiceSid: params.messagingServiceSid
+				};   
+			} else {
+				var body = {		
+						To: params.to, 
+						From: params.from, 
+						Body: params.message  
+				};
+			}
+			
+			var options = {
+					method: 'POST',
+					url: url,
+					form: body,
+					headers: {
+						'Content-Type': 'application/json'
+					}
+			};
+
+			request(options, function(err, res, body) {
+				if (!err && res.statusCode == 201) {
+					var parsedBody = JSON.parse(body);
+					return whisk.done(parsedBody);
+
+				} else {
+					return whisk.error({
+						statusCode: (res || {}).statusCode,
+						error: err,
+						body: body
+					});
+				}
+			});
+		}
 	});
-	
+
 	return whisk.async();
 } 
+
+/**
+ *  A function that check whether the parameters passed are required or not
+ *
+ * @param      {object}    params    An object contains the parameter required
+ *                                   in order to check it and generate a string
+ *                                   that contains a list of missing parameters
+ * @param      {Function}  callback  the callback function has the generated
+ *                                   array or an empty one if the params is
+ *                                   empty or nothing is missing
+ */
+function checkParameters(params, requiredParams, callback) {
+	console.log("Checking Existence of Required Parameters");
+	var missingParams = [];
+	for (var i = requiredParams.length - 1; i >= 0; i--) {
+		console.log(requiredParams[i]);
+		if (!params.hasOwnProperty(requiredParams[i])) {
+			missingParams.push(requiredParams[i]);
+		}
+		if (i == 0)
+			return callback(missingParams);
+	}
+}
