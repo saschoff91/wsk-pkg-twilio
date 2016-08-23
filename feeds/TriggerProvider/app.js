9495e4286271576be94f3b83aa088aae56c0e8ac
saschoff91 <saschoff@de.ibm.com>
2016-08-23 13:33:29 +0200

    Add files via upload

--- feeds/TriggerProvider/app.js
+++ feeds/TriggerProvider/app.js
@@ -174,9 +174,6 @@ app.delete('/deleteFeed/:id', isAuthenticated,function(req,res) {
 		res.status(404).json({
 			error: 'trigger ' + id + ' not found'
 		});
-
-
-
 });
 
 function handleTriggerDeletion(id) {
@@ -234,6 +231,21 @@ app.post('/messageincoming',function(req,res) {
 		res.send(JSON.stringify({result:response}));
 	}
 });
+
+app.post('/messagingservice',function(req,res) {
+	var method = "POST/ messagingservice";
+	var args = typeof req.body === 'object' ? req.body : JSON.parse(req.body);
+	//console.log(req.body);
+	for (var id in feeds) {
+		var val = feeds[id];
+
+		if (val.messagingService == req.body.MessagingServiceSid) {
+			var response = invokeWhiskAction(id, req.body);
+		}
+	}
+	res.send(JSON.stringify({result:response}));
+});
+
 
 /**
  * Fire the whisk trigger
